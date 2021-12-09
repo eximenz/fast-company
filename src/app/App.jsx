e75070390302cad3a59355c039b6ae8d74c0641a
eximenz <terpigoreva1992@gmail.com>
2021-12-09 15:24:07 +0300

    added User component and finish completely App-file

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,69 +1,13 @@
 import React, { useState } from 'react';
-// import Users from './components/users';
+import Users from './components/users';
 import SearchStatus from './components/searchStatus';
 import api from './api';
 
 const App = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
-  const columnNames = ["Имя", "Качества", "Профессия", "Встретился,раз", "Оценка", ""];
 
   const handleDelete = (userId) => {
     setUsers(prevState=>prevState.filter(user=>user!==userId));
-  };
-
-  const renderColumnNames = () => {
-    return columnNames.map((columnNames) => (
-      <th
-        key={columnNames}
-      >
-        {columnNames}
-      </th>
-    ))
-  };
-
-  const getQualityClasses = (color) => {
-
-    let classes = `badge bg-${color} m-2`;
-    return classes;
-  };
-
-  const renderUsers = () => {
-    return users.map((user) => (
-      <tr
-        key={user._id}
-      >
-        <td>
-          {user.name}
-        </td>
-        <td>
-          {user.qualities.map((quality) => {
-            return <span 
-              key={quality._id}
-              className={getQualityClasses(quality.color)}
-            >
-              {quality.name}
-            </span>
-          })}
-        </td>
-        <td>
-          {user.profession.name}
-        </td>
-        <td>
-          {user.completedMeetings}
-        </td>
-        <td>
-          {user.rate}
-        </td>
-        <td>
-          <button
-            className="btn btn-danger btn-sm m-2"
-            onClick={()=>handleDelete(user)}
-          >
-            delete
-          </button>
-        </td>
-      </tr>
-    ))
   };
 
   if (users.length === 0) {
@@ -77,16 +21,10 @@ const App = () => {
       <SearchStatus 
         length = {users.length}
       />
-      <table className="table">
-        <thead>
-          <tr>
-            {renderColumnNames()}
-          </tr>
-        </thead>
-        <tbody>
-          {renderUsers()}
-        </tbody>
-      </table>
+      <Users 
+        users = {users}
+        onHandleDelete = {handleDelete}
+      />
     </>
   );
 };
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -2,9 +2,76 @@ import React from 'react';
 // import User from '../user';
 
 const Users = ({users, ...rest}) => {
+
+  const columnNames = ["Имя", "Качества", "Профессия", "Встретился,раз", "Оценка", ""];
+
+  const renderColumnNames = () => {
+    return columnNames.map((columnNames) => (
+      <th
+        key={columnNames}
+      >
+        {columnNames}
+      </th>
+    ))
+  };
+
+  const getQualityClasses = (color) => {
+
+    let classes = `badge bg-${color} m-2`;
+    return classes;
+  };
+
+  const renderUsers = () => {
+    return users.map((user) => (
+      <tr
+        key={user._id}
+      >
+        <td>
+          {user.name}
+        </td>
+        <td>
+          {user.qualities.map((quality) => {
+            return <span 
+              key={quality._id}
+              className={getQualityClasses(quality.color)}
+            >
+              {quality.name}
+            </span>
+          })}
+        </td>
+        <td>
+          {user.profession.name}
+        </td>
+        <td>
+          {user.completedMeetings}
+        </td>
+        <td>
+          {user.rate}
+        </td>
+        <td>
+          <button
+            className="btn btn-danger btn-sm m-2"
+            onClick={()=>rest.onHandleDelete(user)}
+          >
+            delete
+          </button>
+        </td>
+      </tr>
+    ))
+  };
+
   return (
     <>
-
+      <table className="table">
+        <thead>
+          <tr>
+            {renderColumnNames()}
+          </tr>
+        </thead>
+        <tbody>
+          {renderUsers()}
+        </tbody>
+      </table>
     </>
   );
 };
