a8dc98868f3d01063320f413be5f02434939cf39
eximenz <terpigoreva1992@gmail.com>
2022-02-01 15:19:23 +0300

    added comments part

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import UserCard from "../../ui/userCard";
 import QualitiesCard from "../../ui/qualitiesCard";
 import MeetingsCard from "../../ui/meetingsCard";
+import Comments from "../../ui/comments";
 
 const UserPage = ({ id }) => {
   const [userInfo, setUserInfo] = useState();
@@ -16,14 +17,16 @@ const UserPage = ({ id }) => {
     console.log(userInfo);
     return (
       <>
-        <div className="contai">
+        <div className="container">
           <div className="row gutters-sm">
             <div className="col-md-4 mb-3">
               <UserCard user={userInfo} />
               <QualitiesCard data={userInfo.qualities} />
               <MeetingsCard value={userInfo.completedMeetings} />
             </div>
-            <div className="col-md-8"></div>
+            <div className="col-md-8">
+              <Comments />
+            </div>
           </div>
         </div>
       </>
